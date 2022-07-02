52356641d7cc92903f94dd0b1b54d2f5a2697a85
marees7 <53978254+marees7@users.noreply.github.com>
2022-07-02 11:37:23 +0530

    Change to test

--- api/index.go
+++ api/index.go
@@ -5,6 +5,6 @@ import (
 	"net/http"
 )
 
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
+func HandlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World from Go!")
 }
